6eb265aa22f10cd3c0950a3297dc02477c9d7bb7
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2022-03-30 18:04:28 -0300

    Removed build dir

--- build/animated-title.d.ts
+++
@@ -1,5 +0,0 @@
-import { LitElement } from "lit";
-export declare class AnimatedTitle extends LitElement {
-    render(): import("lit-html").TemplateResult<1>;
-}
-//# sourceMappingURL=animated-title.d.ts.map
