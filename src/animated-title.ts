e708e8fe411860d2ffa275bdfa718db10ed76f27
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2022-03-31 17:31:37 -0300

    Me vuelvo loco anduvo gsap 🥳

--- src/animated-title.ts
+++ src/animated-title.ts
@@ -1,12 +1,34 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, PropertyValueMap } from "lit";
 import { customElement } from "lit/decorators.js";
+import gsap from "gsap";
 
 @customElement("animated-title")
 export class AnimatedTitle extends LitElement {
+  static override get properties() {
+    return {
+      textContent: { type: String },
+    };
+  }
+
+  protected override firstUpdated(
+    _changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>
+  ): void {
+    const algo = this.shadowRoot?.querySelector(".animated-title");
+    if (algo) {
+      gsap.to(algo, {
+        y: 200,
+      });
+    }
+    console.log(algo);
+  }
+
   override render() {
-    return html`
-      <h1>Lenny</h1>
-      <h2>was here tho</h2>
-    `;
+    return html` <h1 class="animated-title">${this.textContent}</h1> `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "animated-title": AnimatedTitle;
   }
 }
